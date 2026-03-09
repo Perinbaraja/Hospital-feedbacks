2d4006705a4c4363c227085c4fc9f8d57ef6eb07
Perinbaraja <102756719+Perinbaraja@users.noreply.github.com>
2026-03-09 12:59:57 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://hospital-feedbacks.onrender.com',
         changeOrigin: true,
       },
     },
