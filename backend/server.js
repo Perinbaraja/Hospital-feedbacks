17ffbebfeb4fbf00d6ccef489b3cc43a6ec0b07f
Perinbaraja <102756719+Perinbaraja@users.noreply.github.com>
2026-03-12 23:47:03 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -38,9 +38,9 @@ if (!process.env.JWT_SECRET) {
 }
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true
+    origin: "*"
 }));
+
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
