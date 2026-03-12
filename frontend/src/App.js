d4cb425e2cbb6c4bd69eb1bff2393b3ad537ef5a
VIMAL K <vimalk007selvi@gmail.com>
2026-03-12 14:10:14 +0530

    Refactor routes in App.js for better organization

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,30 +5,23 @@ import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import CreateForm from './pages/CreateForm';
 import PublicFeedback from './pages/PublicFeedback';
+import FeedbackList from './pages/FeedbackList';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/create-form"
-          element={
-            <ProtectedRoute>
-              <CreateForm />
-            </ProtectedRoute>
-          }
-        />
+        {/* Public routes */}
+        <Route path="/" element={<PublicFeedback />} />
         <Route path="/feedback/:formId" element={<PublicFeedback />} />
-        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<Login />} />
+
+        {/* Admin routes */}
+        <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+        <Route path="/feedbacks" element={<ProtectedRoute><FeedbackList /></ProtectedRoute>} />
+        <Route path="/create-form" element={<ProtectedRoute><CreateForm /></ProtectedRoute>} />
+
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
   );
